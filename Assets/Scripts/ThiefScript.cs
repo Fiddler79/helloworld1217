d76b876e16b6aa08ee94a9e674d42edfb14e5348
Andrew Vermeulen <ethise7@gmail.com>
2015-11-30 21:09:49 +0200

    Random Brontos whack a mole

---
+++ Assets/Scripts/ThiefScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThiefScript : MonoBehaviour {
+
+	private Animator anim;
+	public GameObject[] dinoPrefabArray;
+
+	void Start() {
+		anim = GetComponent<Animator>();
+	}
+
+	void Update () {
+
+		foreach (GameObject thisDino in dinoPrefabArray) {
+			if (isTimeToSpawn (thisDino)) {
+				anim.SetTrigger  ("Bring the dino");
+			}
+		}	
+
+	}
+
+	void OnMouseDown () {
+		anim.SetTrigger  ("Playerhithead");
+	}
+
+	
+	bool isTimeToSpawn (GameObject DinoGameObject) {
+			
+		float meanSpawnDelay = 7.5f;
+		float spawnsPerSecond = 1 / meanSpawnDelay;
+		
+		if (Time.deltaTime > meanSpawnDelay) {
+			Debug.LogWarning ("Spwan rate capped by frame rate");
+		}
+		
+		float threshold = spawnsPerSecond * Time.deltaTime / 5;
+		
+		return (Random.value < threshold);
+	}
+}
